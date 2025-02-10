f65b66fc0e1b476552a04cbf68a3215decf99e11
Jekabs <jekabsporietis@gmail.com>
2025-02-10 03:56:55 +0000

    cleanup of routes

--- vendon/routes.php
+++ vendon/routes.php
@@ -14,14 +14,3 @@ SimpleRouter::all('/quiz/post', 'QuizController@post');
 
 SimpleRouter::all('/result', 'ResultController@index');
 SimpleRouter::all('/result/get', 'ResultController@getAssoc');
-
-
-
-/** Uzstādīšanas Ceļi */
-SimpleRouter::all('/setup', 'SetupController@index');
-SimpleRouter::all('/setup/credentials', 'SetupController@addCredentials');
-SimpleRouter::all('/createDatabase', '\Vendon\Code\Model\Database\CreateDatabase@create');
-SimpleRouter::all('/addTables', '\Vendon\Code\Model\Database\CreateDatabase@addTables');
-SimpleRouter::all('/seedData', '\Vendon\Code\Model\Database\CreateDatabase@seedData');
-
-
