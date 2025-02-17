141064ce2c3ee78c19eef2d7d2818b82be911925
Jekabs <jekabsporietis@gmail.com>
2025-02-17 19:44:13 +0000

    A bit of cleanup

--- vendon/Code/Model/App/Score.php
+++ vendon/Code/Model/App/Score.php
@@ -21,8 +21,7 @@ class Score extends Model
 
     public function find(mixed $data)
     {
-        $userId = $data['user_id'];
-        $quizId = $data['quiz_id'];
+        ['user_id' => $userId, 'quiz_id' => $quizId] = $data;
         $sql = "SELECT id, user_id, quiz_id, score FROM $this->table WHERE user_id = $userId  AND quiz_id = $quizId";
         $conn = Database::newConnection();
         $result = $conn->query($sql);
